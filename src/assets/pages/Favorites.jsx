b16dfd49f3cadcf347c851be779738f73aff3278
dianakius <eingidiana@gmail.com>
2025-08-07 16:52:34 +0300

    feat: create Favorites file, and wrap App in BrowserRouter

---
+++ src/assets/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>Your Favorites</h2>
+        <p>You have no favorite towels yet. Start adding some!</p>
+    </div>
+}
+
+export default Favorite;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    {" "}
+    <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
