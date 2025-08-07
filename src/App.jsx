6c10deb0efb50e3492ec75b0d6f626a5f80e1129
dianakius <eingidiana@gmail.com>
2025-08-07 17:00:16 +0300

    fix(index.html): from tsx to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import ListGroup from "./components/ListGroup";
+import Favorites from "./assets/pages/Favorites";
 import TowelsCard from "./components/TowelsCard";
 import Home from "./assets/pages/Home";
 import "./App.css";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Home/>
-      <TowelsCard
-        towels={{
-          name: "Soft Cotton Towel",
-          url: "https://example.com/towel.jpg",
-          description: "A soft and absorbent cotton towel.",
-        }}
-      />
-      <TowelsCard
-        towels={{
-          name: "Quick-Dry Towel",
-          url: "https://example.com/towel2.jpg",
-          description: "A quick-drying towel perfect for travel.",
-        }}
-      />
-      <TowelsCard
-        towels={{
-          name: "Silk Towel",
-          url: "https://example.com/towel3.jpg",
-          description: "A luxurious silk towel for a luxurious experience.",
-        }}
-      />
-    </>
-  );
-}
+    <main className="main-content">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/towels" element={<TowelsCard />} />
+    </Routes>
+    </main>
+    );
+    }
+    
 
 export default App;
